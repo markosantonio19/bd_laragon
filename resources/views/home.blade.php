cdb9c0a1abe6ba4abe0d1b8f948fcb7babcb9459
markosantonio19 <1433811@senati.pe>
2023-10-19 17:01:09 -0500

    agg crud

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">PRODUCTOS</div>
+                <div class="card-header">Productos</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,11 +14,38 @@
                         </div>
                     @endif
 
-                    productos en venta
+                    <?php
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nombre']) && isset($_POST['precio'])) {
+                        $nombre = $_POST['nombre'];
+                        $precio = $_POST['precio'];
+
+                        $conn = new mysqli("localhost", "usuario", "contrasena", "laravel");
+
+                        if ($conn->connect_error) {
+                            die("Conexión fallida: " . $conn->connect_error);
+                        }
+
+                        $sql = "INSERT INTO productos (nombre, precio) VALUES ('$nombre', $precio)";
+                        
+                        if ($conn->query($sql) === TRUE) {
+                            echo "Producto agregado con éxito";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+
+                        $conn->close();
+                    }
+                    ?>
+
+                    <form method="POST" action="">
+                        Nombre: <input type="text" name="nombre">
+                        Precio: <input type="text" name="precio">
+                        <input type="submit" value="Agregar Producto">
+                    </form>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
-
 @endsection
